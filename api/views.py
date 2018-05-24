34703aa8405a0eaf6a50f793c8e083996187e726
TheElmo <jelmer.internet@xs4all.nl>
2018-05-24 14:08:36 +0200

    course, courses added to path

--- api/views.py
+++ api/views.py
@@ -1,5 +1,21 @@
+import json
+from pprint import pprint
+
+from django.core import serializers
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from api import models
+
 
 def index(request):
     return HttpResponse("Dit is een API")
+
+
+def get_courses(request):
+    response = models.Course.objects.all()
+    return HttpResponse(serializers.serialize('json', response))
+
+def get_course(request):
+    response = models.Course.objects.filter(id=1)
+    return HttpResponse(serializers.serialize('json', response))
